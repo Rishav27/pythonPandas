be4dddb525e20476a2ed35dbc310ae2d94f1da9f
RishavRoshan <rishabhroshan27@gmail.com>
2026-01-18 02:49:34 +0530

    v0.1.2(DataFrame start)

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ def main():
 
 
 if __name__ == "__main__":
-    data = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Chalizard"]
+    data = {"name": ["Alice", "Bob", "Charlie"], "age": [25, 30, 35]}
+    df = pd.DataFrame(data, index=["employee1", "employee2", "employee3"])
 
-    series = pd.Series(data, index=["1", "2", "3", "4", "5", "6"])
-
-    print(series)
+    # add_a_new_column
+    df["job"] = ["Engineer", "Doctor", "Artist"]
+    # add_a_new_row
+    new_row = pd.DataFrame(
+        [{"name": "David", "age": 28, "job": "Lawyer"}], index=["employee4"]
+    )
+    df = pd.concat([df, new_row])
+    print(df)

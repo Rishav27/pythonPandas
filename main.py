cda0c6ea6114a3949f5cffda8af899a83eacbc7f
RishavRoshan <rishabhroshan27@gmail.com>
2026-01-18 02:41:24 +0530

    v0.1.1(pd series complete)

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import pandas as pd
+
+
 def main():
     print("Hello from pandaspython!")
 
 
 if __name__ == "__main__":
-    main()
+    data = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Chalizard"]
+
+    series = pd.Series(data, index=["1", "2", "3", "4", "5", "6"])
+
+    print(series)

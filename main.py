0c30696a752179bf52a579eb2a3e233bae43d45f
RishavRoshan <rishabhroshan27@gmail.com>
2026-01-18 03:24:22 +0530

    v0.1.4(selection using pandas)

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ def main():
 
 
 if __name__ == "__main__":
-    # df = pd.read_csv("data.csv")
+    df = pd.read_csv("data/data.csv", index_col="Name")
 
-    df = pd.read_json("data.json")
-    print(df.to_string())
+    # df = pd.read_json("data/data.json")
+    n = input("Enter a pokimon name:")
+    try:
+        print(df.loc[n])
+    except KeyError:
+        print("Pokimon not found")
+
